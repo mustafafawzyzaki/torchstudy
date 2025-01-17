761d033cb1f5728cd7ff97d6c1bfb46607c0c0a9
mustafafawzyzaki <mustafafawzyzaki@gmail.com>
2025-01-17 18:39:18 +0200

    V1.1

--- change_SUID.py
+++ change_SUID.py
@@ -10,4 +10,8 @@ for root , dirs, files in os.walk(study_directory):
     for file in files:
         if file.endswith(".dcm"):
             ds = pydicom.dcmread(os.path.join(root, file))
-            print (ds)
+            print (ds[0x0020,0x000D])
+            if ds[0x0020,0x000D].value != None:
+                ds[0x0020,0x000D].value = "1.3.6.1.4.1.5962.1.2.0.1175775772.5717.0"
+                ds.save_as(os.path.join(root, file))
+                print (ds[0x0020,0x000D])
