02fc30c430a4b9c67c192bddf32a1a9a484c0938
mustafafawzyzaki <mustafafawzyzaki@gmail.com>
2025-01-17 18:52:53 +0200

    v1.2

--- change_SUID.py
+++ change_SUID.py
@@ -4,14 +4,21 @@ import pydicom
 import pynetdicom 
 
 
-study_directory  = input("Enter the directory where the study is located: ")
+def change_suid(study_directory , new_suid):
+    for root , dirs, files in os.walk(study_directory):
+        for file in files:
+            if file.endswith(".dcm"):
+                ds = pydicom.dcmread(os.path.join(root, file), force=True)
+                if (0x0020, 0x000D) in ds:
+                    if ds[0x0020, 0x000D].value != new_suid:
+                        print(f"Study Instance UID for {file} is not {new_suid}. Updating...")
+                        print(ds[0x0020, 0x000D])
+                        ds[0x0020,0x000D].value = new_suid
+                        # Ensure only the Study Instance UID is converted to a string
+                        ds[0x0020, 0x000D].value = str(ds[0x0020, 0x000D].value)
+                        ds.save_as(os.path.join(root, file))
+                        print(f"Updated Study Instance UID for {file}: {ds[0x0020,0x000D].value}")
 
-for root , dirs, files in os.walk(study_directory):
-    for file in files:
-        if file.endswith(".dcm"):
-            ds = pydicom.dcmread(os.path.join(root, file))
-            print (ds[0x0020,0x000D])
-            if ds[0x0020,0x000D].value != None:
-                ds[0x0020,0x000D].value = "1.3.6.1.4.1.5962.1.2.0.1175775772.5717.0"
-                ds.save_as(os.path.join(root, file))
-                print (ds[0x0020,0x000D])
+study_directory = r"D:\VNA_Test\DICOM_Convert"  
+suid = "1.3.6.1.4.1.5962.1.2.0.1175775772.5717.0"  
+change_suid(study_directory , suid)                 
